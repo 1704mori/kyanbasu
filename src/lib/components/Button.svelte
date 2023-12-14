0406fb99b12e278dc3734ee7ab6cd6e4299bc00c
mori1704 <1704mori@gmail.com>
2023-12-14 13:59:02 -0300

    Update Button component with ghost button functionality

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -5,7 +5,9 @@
 	type Colors = keyof typeof TailwindColors;
 
 	export let type: 'button' | 'submit' | 'reset' = 'button';
-  export let ghost = false;
+  // phantasm is a ghost button that is not visible until hovered
+  export let ghost: "phantasm" | "ghost" = "ghost";
+
 	export let size: 'sm' | 'md' | 'lg' = 'md';
 	export let color: Colors = 'blue';
 	export let loading = false;
@@ -23,18 +25,22 @@
   {...$$props}
   class={cn(
     sizes[size],
+    `${ghost}`,
+    // "bg-blue-600 hover:bg-blue-700 hover:bg-blue-600/20 bg-blue-600/50 border-blue-600",
     `bg-${color}-600 hover:bg-${color}-700 text-white`,
-    ghost && `bg-transparent border border-${color}-600 hover:bg-${color}-600/20 text-white`,
+    ghost === "phantasm" && `bg-transparent border border-${color}-600 hover:bg-${color}-600/20 text-white`,
+    ghost === "ghost" && `bg-${color}-600/20 hover:bg-${color}-600/30 border border-${color}-600`,
     loading && `bg-${color}-600/50 cursor-not-allowed`,
     "rounded-lg",
     "font-medium",
     "transition-colors",
     "focus:outline-none",
     "focus:ring-2",
-    "focus:ring-blue-500",
+    "focus:ring-${color}-500",
     "focus:ring-opacity-50",
     `disabled:bg-${color}-600/50`,
     "disabled:cursor-not-allowed",
+    "disabled:pointer-events-none",
   )}
 	disabled={loading || disabled}
   on:click
